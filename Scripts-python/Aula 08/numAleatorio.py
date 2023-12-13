9b63d20a9e86dfcaaa71f547e96d2d5e91bcf5fd
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-13 14:14:49 -0300

    docs: ataulizações com base nas aulas e um módulo python instalado e testado.

---
+++ Scripts-python/Aula 08/numAleatorio.py
@@ -0,0 +1,11 @@
+#Números aleatórios
+
+import random
+
+#Inves de um usuario digitar um número, o random.random() vai digitar um número aleatório.
+
+#numero int de 1 a 77
+num = random.randint(1, 77)
+
+#O random escreve um numero aleatório (float) entre 0 e 1 
+print(num)
