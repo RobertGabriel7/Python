58b11fe0c6c156418017aa517bb8c360264c2f47
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-19 20:24:06 -0300

    docs: mais atualizações

---
+++ Exercicios/ex025.py
@@ -0,0 +1,22 @@
+nome = str(input('Digite o seu nome: '))
+nome1 = nome.lower()
+
+
+nome2 = ('silva' in nome1)
+
+#print(nome2)
+
+nomeCert = 'SILVA'
+
+#True precisa ser MAIUSCULO
+
+if nome2 == True:
+
+    print('O nome digitado é "{}" e tem o nome "{}", sim!'.format(nome, nomeCert))
+
+else:
+
+    print('O nome digitado é "{}" e não tem o nome "{}".'.format(nome, nomeCert))
+
+
+#FEITO GRAÇAS A DEUS!
