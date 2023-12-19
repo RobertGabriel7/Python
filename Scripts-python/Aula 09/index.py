b10acf700c8f555e3de9ede1348f4f83a17ebb90
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-19 11:48:12 -0300

    docs: mais atualizações

--- Scripts-python/Aula 09/index.py
+++ Scripts-python/Aula 09/index.py
@@ -15,3 +15,24 @@ print(frase[9:13])
 #Nesse exemplo abaixo, será exibido do indice 9 até o 20, mas pulando 2 em 2: frase[9:21:2] 
 
 print(frase[9:21:2])
+
+#Quando não indica o inicio, vai começar do zero.
+
+print(frase[:5])
+
+#Semelhante ao exemplo acima, mas ao contrario. Partir do 15 até o ultimo.
+
+print(frase[15:])
+
+#Partir do 9 até o final pulando 3 em 3
+
+print(frase[9::3])
+
+#count('caractere') vai contar a quantidade de caracteres que foi escrito.
+
+print(frase.count('r'))
+
+#no exemplo abaixo, o método vai contar a quantidade de 'r', a partir do indice 0 até o 14 (é considerado apenas o penultimo numero/indice) 
+
+print(frase.count('r',0,15))
+
