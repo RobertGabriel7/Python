2df34e05b8f9a1b64350849f649a97af4825d92c
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-19 11:38:32 -0300

    docs: mais atualizações

--- Scripts-python/Aula 09/index.py
+++ Scripts-python/Aula 09/index.py
@@ -6,3 +6,12 @@ print(frase[9])
 #len(variavel) diz a quantidade de caracteres em uma variavel
 
 print(len(frase))
+
+
+print('\n')
+
+print(frase[9:13])
+
+#Nesse exemplo abaixo, será exibido do indice 9 até o 20, mas pulando 2 em 2: frase[9:21:2] 
+
+print(frase[9:21:2])
