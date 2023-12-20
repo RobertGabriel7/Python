118eaaa289d810f2bc86c33b4013112e7d422853
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-20 14:21:52 -0300

    docs: mais atualizações

--- Exercicios/ex022.py
+++ Exercicios/ex022.py
@@ -1,13 +1,13 @@
 
-nome = str(input('Qual o seu nome: '))
+nome = str(input('Qual o seu nome: ')).strip()
 
-print(' \n "{}" em letras MAIUSCULAS: '.format(nome),nome.upper())
+print('\n"{}" em letras MAIUSCULAS: '.format(nome),nome.upper())
 
-print(' \n "{}" em letras minusculas: '.format(nome),nome.lower())
+print('\n"{}" em letras minusculas: '.format(nome),nome.lower())
 
 nome1 = nome.replace(' ','')
 
-print('Quantidade de caracteres digitados, sem considerar os espaços:',len(nome1))
+print('\nQuantidade de caracteres digitados, sem considerar os espaços:',len(nome1))
 
 
 #Estou separando as palavras em lista para depois selecionar alguma palavra pelo seu indice
@@ -16,6 +16,6 @@ nome2 = nome.split()
 
 #Pego a quantidade de caracteres do indice 0
 
-print('Quantidade de letras do seu primeiro nome: ', len(nome2[0]))
-
+print('\nQuantidade de letras do seu primeiro nome: ', len(nome2[0]))
+print('\n')
 #FEITO GRAÇAS A DEUS!
