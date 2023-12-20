ad42aca7defc7383bd735bf336e601263a5e154a
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-20 13:01:27 -0300

    docs: mais atualizações

---
+++ Exercicios/ex026.py
@@ -0,0 +1,13 @@
+frase = input("Escreva uma frase... ")
+
+
+
+print('A letra "a" apareceu na frase escrita.', frase.count('a'))
+
+
+print('A posição que a letra "a" apareceu primeira vez foi na: {}°'.format(frase.find('a',0,)))
+
+
+print('A posição que a letra "a" apareceu a ultima vez: {}°'.format(frase.rfind('a')))
+
+#FEITO GRAÇAS A DEUS!
