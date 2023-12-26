5266256cdabe522cfe790b111d46cbf42c1eeb54
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-26 18:10:27 -0300

    Docs

---
+++ Exercicios/ex035.py
@@ -0,0 +1,4 @@
+reta1 = int(input('Digite o comprimento da primeiro reta: '))
+reta2 = int(input('Digite o comprimento da segunda reta: '))
+reta3  = int(input('Digite o comprimento da terceira reta: '))
+
