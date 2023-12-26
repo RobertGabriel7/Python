5a3d17cbc3c76b62728e7e84b106307367b55892
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-26 14:37:55 -0300

    Docs

---
+++ Scripts-python/Aula 10/index.py
@@ -0,0 +1,22 @@
+tempo = int(input("Quantos anos tem o seu carro? "))
+
+""" If simplifcado """
+print('Carro novo' if tempo <= 3 else print('Carro seminovo'))
+
+ 
+""" Tem que tomar cuidade com os espaços na estrutura dos metodos """
+if tempo <= 3:
+
+    print("Carro novo")
+
+else: 
+    
+ if tempo and tempo <= 5:
+   
+    print("Carro seminovo")
+
+ else:
+
+    print("Seu carro é velho")
+
+print('--- FIM ---')
