d79f65247f7a11d4b24039cfee37cc9b068b18f1
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-05-13 13:33:48 -0300

    Updates...

--- Dio/ExplorandoConjuntos/conjuntos.py
+++ Dio/ExplorandoConjuntos/conjuntos.py
@@ -8,6 +8,7 @@ Iteravel = É aquilo que se repete ou é feito outra vez.
 
 """
 
+
 numeros = [1, 2, 3, 4, 5, 5, 7, 1, 1]
 
 """ Remove elementos repetidos de uma lista e devolve em uma class """
@@ -36,8 +37,12 @@ for elementos in carros:
 
 """ Para saber o indice que for utilizado o "for" """
 #for variavel para receber o indice, variavel para receber os valores da list, object, set carros
-for indice, carro in carros:
-    print(f"Indice: {indice}, Carro: {carro}")
+
+carros1 = ("palio", "gol", "celta", "palio")
+
+for carro in carros1:
+    
+    print(f"Indice: , Carro: {carro}")
     
 
 """ Métodos da class Set """
@@ -47,4 +52,52 @@ for indice, carro in carros:
 conjuntoA = {1, 2}
 conjuntoB = {3, 4}
 
-print(conjuntoA.union(conjuntoB)) # 
+print(conjuntoA.union(conjuntoB)) # {1, 2, 3, 4}
+
+""" 
+Interseguição é a parte do conjunto que são iguais. 
+
+"""
+
+conjuntoA1 = {1, 2, 3}
+conjuntoB1 = {2, 3, 4}
+
+print(conjuntoA1.intersection(conjuntoB1)) # {2, 3}
+
+""" difference é a diferença entre os valores do conjunto """
+
+print(conjuntoA1.difference(conjuntoB1)) # {1}
+print(conjuntoB1.difference(conjuntoA1)) # {4}
+
+""" symmetric_difference pega todos os elementos que não estão interseguição.  Esse método é melhor do que o diference """
+
+print(conjuntoA1.symmetric_difference(conjuntoB1))
+
+""" issubset serve para saber se um conjunto faz parte de outro conjunto """
+
+conjuntoB1 = {4, 1, 2, 5, 6, 3}
+
+#Todos os elementos de A1 estão em B1? True 
+print(conjuntoA1.issubset(conjuntoB1)) # True
+# TOdos os elementos de B1 estão em A1? False
+print(conjuntoB1.issubset(conjuntoA1)) # False
+
+""" issuperset é o contrario do issubset """
+
+# Todos os elementos de B1 estão em A1? False
+conjuntoA1.issuperset(conjuntoB1) # False
+# Todos os elementos de A1 estão em B1? True
+conjuntoB1.issuperset(conjuntoA1) # True
+
+""" isdisjoint serve para saber se todos ou alguns elementos de outro conjunto estão em outro conjunto """
+
+conjuA = {1,2,3,4,5}
+conjuB = {6,7,8,9}
+conjuC = {1,0}
+
+# Se acontecer a interseguição, será retornado False
+print(conjuA.isdisjoint(conjuB)) # True
+print(conjuA.isdisjoint(conjuC)) # False 
+
+""" Para adicionar elementos/valores não existente: {}.add """
+
