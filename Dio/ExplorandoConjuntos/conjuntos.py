0a4d9dca09fb46f31b71624911cebd8bbfe83b39
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-05-13 12:37:32 -0300

    Updates...

--- Dio/ExplorandoConjuntos/conjuntos.py
+++ Dio/ExplorandoConjuntos/conjuntos.py
@@ -15,4 +15,36 @@ print(set(numeros)) # {1, 2, 3, 4, 5, 7}
 
 print(set("abacaxi")) # {'a', 'i', 'x', 'b', 'c'}
 
-print(("palio", "gol", "celta", "palio")) # ('palio', 'gol', 'celta', 'palio')
+carros = set(("palio", "gol", "celta", "palio"))
+
+print(carros) # ('palio', 'gol', 'celta', 'palio')
+
+
+""" Para acessar os elementos do set, precisa converte para uma lista """
+
+opa = list(set(numeros))
+
+print(opa[0])
+
+
+""" Para percorrer os elementos de uma coleção (set) """
+
+
+for elementos in carros:
+    print(elementos)
+
+
+""" Para saber o indice que for utilizado o "for" """
+#for variavel para receber o indice, variavel para receber os valores da list, object, set carros
+for indice, carro in carros:
+    print(f"Indice: {indice}, Carro: {carro}")
+    
+
+""" Métodos da class Set """
+
+""" Para unir coleções: {}.union """
+
+conjuntoA = {1, 2}
+conjuntoB = {3, 4}
+
+print(conjuntoA.union(conjuntoB)) # 
