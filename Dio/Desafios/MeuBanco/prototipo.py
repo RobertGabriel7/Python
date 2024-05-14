c20b655bd9f46080abdf303dee4dfa9bf2d5a2d2
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-05-14 07:55:42 -0300

    Sistema bancario atualizado

--- Dio/Desafios/MeuBanco/prototipo.py
+++ Dio/Desafios/MeuBanco/prototipo.py
@@ -1,43 +1,57 @@
 # Desafio: Sacar, depositar e visualizar o histórico
 
+import string
+
+
 saldo = 100
-limiteDeSaques = 0
+limiteDeSaques = 1
 desejaContinuar = 'y'
 
-# Function para sacar valores
+# Sacar valores
 def sacar(valorDoSaque):
 
     if (limiteDeSaques > 3):
-        print("Voce somente pode fazer 3 saques por dia!")
+        return print("Voce somente pode fazer 3 saques por dia!")
 
     else:
-        resultado = saldo - valorDoSaque        
-        return print(f"Saldo atual: R${resultado}")
-limiteDeSaques += 1
-
+        global saldo 
+        saldo -= valorDoSaque        
+        return print(f"Saldo atual: R${saldo}")
 
 # Depositar valores
 def depositar(deposito):
-    resultado = saldo + deposito
-    return (resultado)
+    global saldo
+    saldo += deposito
+    return print(f"Saldo atual: R${saldo}")
 
+# Consulta de valores
+def extrato():
+    print(f"Voce tem R${saldo}.")
 while (desejaContinuar == 'y'):
-
-    opcao = int(input("Digite uma opcao: "))
+    print("\n ------------- Bem-vindo(a) ao Seven ------------- \n")
+    print("\nQual das opcoes a seguir vc deseja fazer: \n\n[1].Sacar \n\n[2].Depositar \n\n[3].Consulta de Saldo \n\n[4].Sair\n")
+    
+    opcao = int(input("Digite apenas uma opcao: "))
+    print(limiteDeSaques)
     
     if (opcao == 1):
         
         valorDoSaque = int(input(f"Quanto voce desejar sacar de R${saldo} ? R$"))
         if (valorDoSaque > saldo):
-            print(f"O valor diigitado foi superior ao valor disponivel!")
+            print(f"\nO valor diigitado foi superior ao valor disponivel!\n")
             desejaContinuar = str(input("Deseja continuar: (y/n)"))
         else:
             sacar(valorDoSaque)
+            limiteDeSaques += 1
             desejaContinuar = str(input("Deseja continuar: (y/n)"))
 
     elif (opcao == 2):
-        deposito = int(input("Quando voce deseja depositar? R$"))
+        deposito = int(input("\nQuando voce deseja depositar? R$"))
         depositar(deposito)
         desejaContinuar = str(input("Deseja continuar: (y/n)"))
-    
-    
+    elif(opcao == 3):
+        extrato()
+        desejaContinuar = str(input("Deseja continuar: (y/n)"))
+    else:
+        print("Opcao invalida")
+        desejaContinuar = str(input("Deseja continuar: (y/n)"))
